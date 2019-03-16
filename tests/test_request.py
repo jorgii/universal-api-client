59529179df9f2f02efe336ae13ed960f1856ebd5
George Goranov <g.p.goranov@gmail.com>
2019-03-17 00:14:16 +0200

    Modify API request to fix stacking url issue

--- tests/test_request.py
+++ tests/test_request.py
@@ -11,7 +11,7 @@ def base_url():
 @pytest.fixture
 def request_instance(base_url):
     """API request fixture"""
-    return APIRequest(base_url=base_url)
+    return APIRequest(url=base_url)
 
 
 def test_url_single_path(request_instance, base_url):
@@ -23,12 +23,21 @@ def test_url_multi_path(request_instance, base_url):
         '{}so/many/people/'.format(base_url)
 
 
-def test_url_single_path_with_param(request_instance, base_url):
+def test_url_single_path_with_identifier(request_instance, base_url):
     assert request_instance.people(identifier=57812).url == \
         '{}people/57812/'.format(base_url)
 
 
-def test_url_multi_path_with_param(request_instance, base_url):
+def test_url_multi_path_with_identifier(request_instance, base_url):
+    assert request_instance.people(identifier=57812).are('so').many.url == \
+        '{}people/57812/are/so/many/'.format(base_url)
+
+
+def test_url_multi_path_with_many_requests(request_instance, base_url):
+    assert request_instance.people(identifier=57812).are('so').many.url == \
+        '{}people/57812/are/so/many/'.format(base_url)
+    assert request_instance.people(identifier=57812).are('so').many.url == \
+        '{}people/57812/are/so/many/'.format(base_url)
     assert request_instance.people(identifier=57812).are('so').many.url == \
         '{}people/57812/are/so/many/'.format(base_url)
 
@@ -36,3 +45,16 @@ def test_url_multi_path_with_param(request_instance, base_url):
 def test_url_single_with_query(request_instance, base_url):
     assert request_instance.people(query={'foo': 'bar'}).url == \
         '{}people/?foo=bar'.format(base_url)
+
+
+def test_url_multi_path_with_query_and_identifier(request_instance, base_url):
+    assert request_instance.people(
+        identifier=123).chances(query={'foo': 'bar'}).url == \
+            '{}people/123/chances/?foo=bar'.format(base_url)
+
+
+def test_url_multi_path_with_mixed_query_and_identifier(request_instance,
+                                                        base_url):
+    assert request_instance.people(
+        identifier=123, query={'foo': 'bar'}).chances.url == \
+            '{}people/123/chances/?foo=bar'.format(base_url)
--- universal_api_client/request.py
+++ universal_api_client/request.py
@@ -2,27 +2,23 @@ from urllib import parse
 
 
 class APIRequest(object):
-    def __init__(self, base_url):
-        self.base_url = base_url
-        self._url = parse.urlparse(base_url)
-
-    @property
-    def url(self):
-        return parse.urlunparse(self._url)
+    def __init__(self, url):
+        self.url = url
 
     def __getattr__(self, name):
-        self._url = self.update_url(self._url, path=name)
-        return self
+        updated_url = self._update_url(self.url, name)
+        return APIRequest(updated_url)
 
     def __call__(self, identifier=None, query=None):
-        self._url = self.update_url(self._url, path=identifier, query=query)
-        return self
+        updated_url = self._update_url(
+            self.url, identifier=identifier, query=query)
+        return APIRequest(updated_url)
 
-    def update_url(self, url, path=None, query=None):
-        url_parts = list(url)
-        if path:
-            url_parts[2] = parse.urljoin(url.path, str(path)) + '/'
+    def _update_url(self, url, identifier=None, query=None):
+        url_parts = list(parse.urlparse(url))
+        if identifier:
+            url_parts[2] = parse.urljoin(url_parts[2], str(identifier)) + '/'
         if query and isinstance(query, dict):
-            query.update(parse.parse_qsl(url.query))
+            query.update(parse.parse_qsl(url_parts[4]))
             url_parts[4] = parse.urlencode(query)
-        return parse.urlparse(parse.urlunparse(url_parts))
+        return parse.urlunparse(url_parts)
